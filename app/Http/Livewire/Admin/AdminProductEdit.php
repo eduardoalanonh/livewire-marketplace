6d47b5ef940a6cd94a005b33834901ac91a9012c
EduardoAlano <43254637+EduardoAlano@users.noreply.github.com>
2020-11-08 22:55:50 -0300

    add photo on product

--- app/Http/Livewire/Admin/AdminProductEdit.php
+++ app/Http/Livewire/Admin/AdminProductEdit.php
@@ -3,10 +3,14 @@
 namespace App\Http\Livewire\Admin;
 
 use App\Models\Product;
+use App\Models\ProductPhoto;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class AdminProductEdit extends Component
 {
+    use WithFileUploads;
+
     public $product;
     public $name;
     public $description;
@@ -14,6 +18,7 @@ class AdminProductEdit extends Component
     public $unique_product;
     public $photo;
     public $s3Photo = 'https://auroramarketplace.s3-sa-east-1.amazonaws.com/';
+    public $photos;
 
 
     public function mount($id)
@@ -23,7 +28,7 @@ class AdminProductEdit extends Component
         $this->description = $this->product->description;
         $this->price = $this->product->price;
         $this->unique_product = $this->product->unique_product;
-        $this->photo = $this->product->photo;
+        $this->photos = $this->product->photo;
     }
 
 
@@ -40,19 +45,43 @@ class AdminProductEdit extends Component
     public function updateProduct()
     {
         $this->product->update([
-           'name' =>  $this->name,
-           'description'  => $this->description,
-           'price'=> $this->price,
-           'unique_product' => $this->unique_product
+            'name' => $this->name,
+            'description' => $this->description,
+            'price' => $this->price,
+            'unique_product' => $this->unique_product
         ]);
 
         $this->alert('success', 'Sucesso!', [
-            'position'  =>  'center',
-            'timer'  =>  5000,
-            'toast'  =>  false,
-            'text'  =>  'Seu produto foi atualizado!',
-            'showCancelButton'  =>  false,
-            'showConfirmButton'  =>  false
+            'position' => 'center',
+            'timer' => 5000,
+            'toast' => false,
+            'text' => 'Seu produto foi atualizado!',
+            'showCancelButton' => false,
+            'showConfirmButton' => false
         ]);
     }
+
+    public function updatePhoto()
+    {
+
+        $path = $this->photo->store('photos', 's3');
+
+        ProductPhoto::create([
+                'product_id' => $this->product->id,
+                'image' =>  $path,
+            ]);
+
+       $this->alert('success', 'Suas fotos foram atualizadas', [
+            'position' => 'center',
+            'timer' => 5000,
+            'toast' => false,
+            'text' => 'Seu produto foi criado!',
+            'showCancelButton' => false,
+            'showConfirmButton' => false
+        ]);
+
+       $this->photo = null;
+
+    }
+
 }
--- resources/views/livewire/admin/admin-product-edit.blade.php
+++ resources/views/livewire/admin/admin-product-edit.blade.php
@@ -54,40 +54,53 @@
                         <div class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
                     </div>
                 </div>
-                <div class="items-center" wire:loading wire:target="updatedPhoto">
-                    <div class="fixed top-0 right-0 h-screen w-screen z-50 flex justify-center items-center">
-                        <div class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
-                    </div>
-                </div>
 
                 <div class="flex justify-center">
-                    <label
-                        class="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue hover:text-white">
-                        <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                            <path
-                                d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z"/>
-                        </svg>
-                        <span class="mt-2 ">Adicione mais imagens aqui</span>
-                        <input type='file' class="hidden"/>
-                    </label>
+                    <div>
+                        <label
+                            class="w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border  cursor-pointer text-center  hover:text-gray-600">
+                            <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
+                                 viewBox="0 0 20 20">
+                                <path
+                                    d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z"/>
+                            </svg>
+                            <span
+                                class="mt-2 text-base leading-normal">Quer adicionar mais fotos no seu produto?</span>
+                            <input type='file' class="hidden" wire:model="photo"/>
+                        </label>
+                    </div>
                 </div>
+                @if($photo)
+                    <div class="flex justify-center mt-5">
+                        <button wire:click="updatePhoto"
+                                class="bg-gray-100 hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center">
+                            <svg class="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/>
+                            </svg>
+                            <span>Clique aqui para carregar a foto</span>
+                        </button>
+                    </div>
+                @endif
 
                 <div class="container mx-auto flex flex-wrap pt-4 pb-12">
-                    <div class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col border-black border-opacity-25 items-center">
+                    @foreach($photos as $productPhoto)
                         <div
-                            style="background-image: url({{$s3Photo . $product->photo[0]['image']}})"
-                            class="bg-gray-300 h-64 w-full rounded-lg shadow-md bg-cover bg-center"
-                            loading="lazy"></div>
-                        <div
-                            class="w-56 md:w-64 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden justify-items-center hover:grow hover:shadow-lg border-black border-opacity-25">
-                            <div class="flex justify-center  py-2 px-3 bg-gray-400">
-                                <a href="{{route('single.product',['id' => $product->id])}}"
-                                   class=" bg-red-800 text-xs text-white px-2 py-1 font-semibold rounded uppercase hover:bg-red-700">
-                                    Excluir
-                                </a>
+                            class="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col border-black border-opacity-25 items-center">
+                            <div
+                                style="background-image: url({{$s3Photo .$productPhoto->image}})"
+                                class="bg-gray-300 h-64 w-full rounded-lg shadow-md bg-cover bg-center"
+                                loading="lazy"></div>
+                            <div
+                                class="w-56 md:w-64 bg-white -mt-10 shadow-lg rounded-lg overflow-hidden justify-items-center hover:grow hover:shadow-lg border-black border-opacity-25">
+                                <div class="flex justify-center  py-2 px-3 bg-gray-400">
+                                    <a href="{{route('single.product',['id' => $productPhoto->product_id])}}"
+                                       class=" bg-red-800 text-xs text-white px-2 py-1 font-semibold rounded uppercase hover:bg-red-700">
+                                        Excluir
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    @endforeach
                 </div>
 
             </div>
